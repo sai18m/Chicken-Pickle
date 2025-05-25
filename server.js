cabe3b4692a3d0800dd360048f98eea36dcecbbb
PRAKASH <104700818+sai18m@users.noreply.github.com>
2025-05-25 11:08:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.post("/submit-address", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
